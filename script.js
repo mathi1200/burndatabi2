474a352d696d86a523199bd8fb5869635852405a
Matheus Campos Oliveira Dutra <79610447+mathi1200@users.noreply.github.com>
2023-12-08 09:50:37 -0300

    Add files via upload
    news update

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-    document.addEventListener('DOMContentLoaded', function () {
-        // Add smooth scroll with snap points
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+    // Adiciona centralização de cada seção ao rolar
+    const sections = document.querySelectorAll('section');
+    
+    sections.forEach((section, index) => {
+        section.style.scrollSnapAlign = 'center';
+    });
 
-                const targetElement = document.querySelector(this.getAttribute('href'));
+    // Adiciona smooth scroll com pontos de ancoragem
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-                if (targetElement) {
-                    targetElement.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'start'  // Ensure alignment to the start of the target
-                    });
-                }
-            });
+            const targetElement = document.querySelector(this.getAttribute('href'));
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'  // Garante que a seção seja centralizada na tela
+                });
+            }
         });
     });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dataPanel = document.getElementById('dataPanel');
+    const expandedImage = document.getElementById('expandedImage');
+    console.log("clique realizada");
+    dataPanel.addEventListener('click', function () {
+        dataPanel.classList.toggle('expanded');
+        document.body.style.overflow = document.body.style.overflow === 'hidden' ? '' : 'hidden'; // Alternar overflow
+    });
+});
